ec6205658ca30ffc8d0e265936894bb478398a5d
liqiong-lab <62087481+liqiong-lab@users.noreply.github.com>
2020-03-19 21:24:14 +0800

    Add files via upload

---
+++ test01/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
+
+
+
+
+
+/*demo04.vue*/
+<template>
+  <div id="demo04">
+    <ul>
+      <demo04 :model="data"></demo04>
+    </ul>
+  </div>
+</template>
+
+<script>
+import demo04 from "./components/demo/vue/add.vue"
+export default {
+  name:'APP',
+  components:{
+    demo04
+  },
+
+  data(){
+    return{
+      data:data
+    }
+  }
+}
+</script>
---
+++ test01/add.vue
@@ -0,0 +1,67 @@
+
+<template>
+  <div id="demo04">
+    <h3 align="center">添加地址</h3>
+    <!-- @submit.prevent 阻止事件的默认行为，当前阻止的是action行为 -->
+    <form action="#"
+          @submit.prevent="submitForm">
+
+      <br><br> 收货人
+      <nut-textinput v-model="name"
+                     placeholder="请输入"
+                     :clearBtn="true"
+                     :disabled="false" />
+      <br><br> 手机号
+      <nut-textinput v-model="number"
+                     placeholder="请输入"
+                     :clearBtn="true"
+                     :disabled="false"
+                     color="red" />
+      <br><br> 所在地区
+      <nut-textinput v-model="area"
+                     placeholder="请输入"
+                     :clearBtn="true"
+                     :disabled="false" />
+      <br><br> 详细地址
+      <nut-textinput v-model="desc"
+                     placeholder="街道 小区 门牌号"
+                     :clearBtn="true"
+                     :disabled="false" />
+      <br><br> 设为默认地址
+      <nut-switch :active.sync="swActive">
+      </nut-switch>
+      <br><br>
+      <nut-button block
+                  :type="submit">
+        保存
+      </nut-button>
+    </form>
+  </div>
+</template>
+
+<script src="https://cdn.staticfile.org/vue/2.2.2/vue.min.js"></script>
+<script src="./node_modules/vue/dist/vue.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.min.js"></script>
+
+<script>
+export default {
+  el: '#demo04',
+  data () {
+    return {
+      swActive: true
+    };
+  },
+  methods: {
+    submitForm: function () { //处理提交表单
+      //发送ajax异步处理
+      alert(this.name + ', ' + this.number + ', ' + this.area + ', ' + this.desc + ',' + this.swActive)
+    }
+  }
+}
+</script>
+
+<style>
+</style>
+
+
+
